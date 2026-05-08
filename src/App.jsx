3378efa65c9d07f96d6840e45d4b7682da329c39
Nityanand Singh <nns.nitya@gmail.com>
2026-05-08 20:53:41 +0530

    toast added

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,21 @@ import './App.css';
 import Header from './components/Header';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button } from 'reactstrap';
+import { ToastContainer, toast } from 'react-toastify';
 
 function App() {
 
+  const btnHandle=() => {
+    toast("Button is clicked.", {
+      position: 'bottom-right'
+    });
+  }
+
   return (
     <div>
+      <ToastContainer/>
       <h1>This is bootstrap Component</h1>
-      <Button color='primary'>React Button</Button>
+      <Button color='primary' onClick={btnHandle}>React Button</Button>
     </div>
   );
 }
