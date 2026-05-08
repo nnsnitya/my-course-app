7a732aeda73aa65fbba4a0b017be2cc9dcaf06a5
Nityanand Singh <nns.nitya@gmail.com>
2026-05-08 20:39:58 +0530

    reactstrap button added

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from './assets/vite.svg';
+import heroImg from './assets/hero.png';
+import './App.css';
 import Header from './components/Header';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'reactstrap';
 
 function App() {
 
   return (
     <div>
-      <Header name = "Nityanand Singh" title = "First Card" />
-      <hr/>
-      <Header name = "Pankaj Saxena" title = "Second Card" />
-      <hr/>
-        <h1>This is my React App</h1>
-        <p>This is my program, I am creating course app</p>
-        <p>Because I want to develop application that calls my Rest API's</p>
-      <hr/>
-      <Header name = "Premshankar Kushwaha" title= "Third Card"/>
+      <h1>This is bootstrap Component</h1>
+      <Button color='primary'>React Button</Button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
