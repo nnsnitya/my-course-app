0ad8c8e6fe7426effd59d96c16e8a82e41a05fac
Nityanand Singh <nns.nitya@gmail.com>
2026-05-10 17:58:42 +0530

    Used Link to resolve Refresh Pages
    while changing the routes whole page was being refreshed,
    by using Link it resolved only content part getting change without refreshing whole page.

--- src/components/LeftMenus.jsx
+++ src/components/LeftMenus.jsx
@@ -1,25 +1,26 @@
 import React from "react";
-import { ListGroup, ListGroupItem } from "reactstrap";
+import { ListGroup} from "reactstrap";
+import { Link } from "react-router-dom";
 
 const LeftMenus = () => {
 
     return (
         <ListGroup flush>
-            <ListGroupItem tag="a" href="/" action>
+            <Link tag="a" to="/" action>
                 Home
-            </ListGroupItem>
-            <ListGroupItem tag="a" href="/add-course" action>
+            </Link>
+            <Link tag="a" to="/add-course" action>
                 Add Course
-            </ListGroupItem>
-            <ListGroupItem tag="a" href="/view-courses" action>
+            </Link>
+            <Link tag="a" to="/view-courses" action>
                 View Courses
-            </ListGroupItem>
-            <ListGroupItem tag="a" href="#!" action>
+            </Link>
+            <Link tag="a" to="#!" action>
                 About
-            </ListGroupItem>
-            <ListGroupItem tag="a" href="#!" action>
+            </Link>
+            <Link tag="a" to="#!" action>
                 Contact
-            </ListGroupItem>
+            </Link>
         </ListGroup>
     );
 };
